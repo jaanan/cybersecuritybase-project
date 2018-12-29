a95e18bec59c7f21152bfb66b272ecc69833e3b5
jaanan <42993030+jaanan@users.noreply.github.com>
2018-12-29 13:37:05 +0200

    Update SignupController.java
    Lisätty 
    
    import org.springframework.data.domain.PageRequest;
    import org.springframework.data.domain.Sort;

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,6 +1,8 @@
 package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
