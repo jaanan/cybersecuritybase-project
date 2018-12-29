70b2734a70dc9bccdb220d561d86abc428cc85dc
jaanan <42993030+jaanan@users.noreply.github.com>
2018-12-29 15:33:52 +0200

    Update SecurityConfiguration.java
    muokattu rivi 43 CustomPasswordEncoder();

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -40,6 +40,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new CustomPasswordEncoder();
     }
 }
