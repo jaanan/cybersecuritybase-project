67790e7955421f1a6c6499592807c6374f4fee46
jaanan <42993030+jaanan@users.noreply.github.com>
2018-12-29 15:43:04 +0200

    Create CustomPasswordEncoder.java

---
+++ src/main/java/sec/project/config/CustomPasswordEncoder.java
@@ -0,0 +1,23 @@
+package sec.project.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+    public String encode(CharSequence rawPassword) {
+        String answer = "";
+        for(int i = 0; i < rawPassword.length(); i ++)
+        {
+            answer = answer + ((int)rawPassword.charAt(i));
+        }
+        return answer;
+    }
+
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        String encodedRaw = this.encode(rawPassword);
+        if(encodedPassword.equals(encodedRaw))
+            return true;
+        else
+            return false;
+    }
+}
