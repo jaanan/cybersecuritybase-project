646dd5edc6d75c8b686006f52b664a242fb68e6f
jaanan <42993030+jaanan@users.noreply.github.com>
2018-12-29 13:48:09 +0200

    Update SignupRepository.java
    added 
    
    import java.util.List;
    
    SignupRepositoryCustom
     ja 
    List<Signup> findByUser(String user);

--- src/main/java/sec/project/repository/SignupRepository.java
+++ src/main/java/sec/project/repository/SignupRepository.java
@@ -3,6 +3,10 @@ package sec.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sec.project.domain.Signup;
 
-public interface SignupRepository extends JpaRepository<Signup, Long> {
+import java.util.List;
 
+public interface SignupRepository extends JpaRepository<Signup, Long>, SignupRepositoryCustom {
+  
+    List<Signup> findByUser(String user); 
+  
 }
